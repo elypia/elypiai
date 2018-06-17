eba33240ff2b6b44d67dee5a6fbd0d91e5395929
Seth <sethsutopia@gmail.com>
2018-06-17 23:40:51 +0100

    - Created little Tuple thingi for future reference

---
+++ src/main/java/com/elypia/elypiai/utils/Tuple.java
@@ -0,0 +1,32 @@
+package com.elypia.elypiai.utils;
+
+public class Tuple<T1, T2> {
+
+    private T1 valueOne;
+    private T2 valueTwo;
+
+    public static <T1, T2> Tuple<T1, T2> of(T1 valueOne, T2 valueTwo) {
+        return of(valueOne, valueTwo);
+    }
+
+    public Tuple(T1 valueOne, T2 valueTwo) {
+        this.valueOne = valueOne;
+        this.valueTwo = valueTwo;
+    }
+
+    public T1 getValueOne() {
+        return valueOne;
+    }
+
+    public void setValueOne(T1 valueOne) {
+        this.valueOne = valueOne;
+    }
+
+    public T2 getValueTwo() {
+        return valueTwo;
+    }
+
+    public void setValueTwo(T2 valueTwo) {
+        this.valueTwo = valueTwo;
+    }
+}
