6a076732dd55dff09f852bed6bcf3eb18c3bef81
Syed Seth <seth@elypia.com>
2019-01-03 00:59:26 +0100

    - Removed all Amazon related code - Fixed issue with Nanowrimo having incorrect timezone

--- nanowrimo/src/main/java/com.elypia.elypiai.nanowrimo/deserializers/DateDeserializer.java
+++ nanowrimo/src/main/java/com.elypia.elypiai.nanowrimo/deserializers/DateDeserializer.java
@@ -2,17 +2,23 @@ package com.elypia.elypiai.nanowrimo.deserializers;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 public class DateDeserializer extends XmlAdapter<String, Date> {
 
-    @Override
-    public Date unmarshal(String v) throws Exception {
-        return new SimpleDateFormat("yyyy-MM-dd").parse(v);
+    private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+    static {
+        format.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
     @Override
-    public String marshal(Date v) throws Exception {
-        return new SimpleDateFormat("yyyy-MM-dd").format(v);
+    public Date unmarshal(String v) throws Exception {
+        return format.parse(v);
+    }
+
+    @Override
+    public String marshal(Date v) {
+        return format.format(v);
     }
 }
