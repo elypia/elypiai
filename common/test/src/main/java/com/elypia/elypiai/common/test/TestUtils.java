8241c61716564e94f8e579f0eaa05f7e9645e529
Seth <seth@elypia.com>
2019-07-09 22:02:01 +0200

    * Address javadoc warning * Updated gradle to 5.5 * Updated dependencies

--- common/test/src/main/java/com/elypia/elypiai/common/test/TestUtils.java
+++ common/test/src/main/java/com/elypia/elypiai/common/test/TestUtils.java
@@ -1,7 +1,6 @@
 package com.elypia.elypiai.common.test;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
@@ -21,6 +20,7 @@ public final class TestUtils {
      *
      * @param name The name of the file to load excluding the extension.
      * @return The content of the file.
+     * @throws IOException If no resource if found at this path.
      */
     public static String read(String name) throws IOException {
         String path = "/" + name;
